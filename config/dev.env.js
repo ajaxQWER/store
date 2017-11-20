d9f6b92488cc6aa00eef615f6c6afb4dd4432be9
xuejiangjun <449590828@qq.com>
2017-11-21 00:57:31 +0800

    修改测试服务器地址已经图片上传地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://apitest.gongxiangdiancan.com"',
-  UPLOAD_URL: '"http://uploadstest.gongxiangdiancan.com"'
+  BASE_URL: '"http://api.test.gongxiangdiancan.com"',
+  UPLOAD_URL: '"http://uploads.test.gongxiangdiancan.com"'
 })
