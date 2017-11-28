373723ea3590847dbd1e49ef3086b1a28e6b7b17
xuejiangjun <449590828@qq.com>
2017-11-28 18:31:05 +0800

    新增测试打包命令相关，打包测试命令npm run test

---
+++ config/test.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production-test"',
+  BASE_URL: '"http://api.test.gongxiangdiancan.com"',
+  UPLOAD_URL: '"http://uploads.test.gongxiangdiancan.com"'
+}
